f02887f332d334fceaaeac0a8d5589999e2a3750
Artur Ardai <krkerdo@gmail.com>
2022-07-22 17:54:27 -1200

    basics

---
+++ basic.py
@@ -0,0 +1,27 @@
+from pickletools import int4
+from sys import int_info
+from turtle import TurtleGraphicsError
+
+from Variables import myfunc
+
+
+print("hello")
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+        #sup man
+"""
+what the  what is TurtleGraphicsError
+"""
+man=10
+work=10
+print(man,work)
+a='jojo' 
+A="jojo";C=1
+a=x=c="man"
+
+fruits=["he","she","banana"]
+x,z,c=fruits
+print(x,z,c)
+myfunc()
