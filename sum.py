086b18faafa2c70d926ce1ad826356c9ff62ffd1
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-08-20 15:02:31 +0300

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,35 @@
+   
+numbers = [1,2,3,4,5,1,4,5]
+  
+# start parameter is not provided
+Sum = sum(numbers)
+print(Sum)
+  
+# start = 10
+Sum = sum(numbers, 10)
+print(Sum)
+
+# Output -
+# 25
+# 35
+
+
+print("enter 2 numbers")
+a =int(input("enter 1 st number"))
+b =int(input("enter 2nd number"))
+c = a+b
+print ("here is your answer",c)
+
+
+prime_numbers = {2, 3, 5, 7}
+
+# add 11 to prime_numbers
+prime_numbers.add(11)
+
+
+print(prime_numbers)
+
+#if the element is already in the set, it won't add it
+
+
+
