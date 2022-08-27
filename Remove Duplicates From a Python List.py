d1714f20bd65682c794e40b5a178c8d3d1ca26b6
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-08-27 19:21:25 +0300

    Create Remove Duplicates From a Python List.py

---
+++ Remove Duplicates From a Python List.py
@@ -0,0 +1,13 @@
+mylist = ["a", "b", "a", "c", "c"]
+mylist = list(dict.fromkeys(mylist))
+print(mylist)
+
+
+
+mylist = ["a", "b", "a", "c", "c"]
+mylist = list(dict.fromkeys(mylist))
+print(mylist)
+
+mylist = ["a", "b", "a", "c", "c"]
+mylist = list( dict.fromkeys(mylist) )
+print(mylist)
